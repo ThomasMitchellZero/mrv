c5d2c452ae2ce95d286e302271655ec7fc7fbe5c
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2022-09-03 15:43:50 -0400

    component now tracks Active and IsDescending states

--- src/components/UI/TableHeading.js
+++ src/components/UI/TableHeading.js
@@ -1,15 +1,34 @@
 import classes from "./TableHeading.module.css";
 import { MdArrowDownward, MdArrowUpward } from "react-icons/md";
+import { useState } from "react";
 
 const TableHeading = (props) => {
-  const downArrow = <MdArrowDownward className={classes.icon}/>;
-  const upArrow = <MdArrowUpward className={classes.icon}/>;
+
+  const [isActive, setIsActive] = useState(false);
+  const [isDescending, setIsDescending] = useState(true);
+
+    // might be worth looking at doing the arrow state as a single object?
+
+  const activeStatus = () => {
+    return isActive ? `${classes.active}` : "";
+  };
+
+  const downArrow = (
+    <MdArrowDownward className={`${classes.icon} ${activeStatus()}`} />
+  );
+  const upArrow = (
+    <MdArrowUpward className={`${classes.icon} ${activeStatus()}`} />
+  );
+
+  const clickHandler = (event) => {
+    console.log(event.target);
+  };
 
   return (
-    <th>
+    <th onClick={clickHandler}>
       <div className={classes.tableheading}>
         <p>{props.children}</p>
-        {downArrow}
+        {isDescending ? downArrow : upArrow}
       </div>
     </th>
   );
