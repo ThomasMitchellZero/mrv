4ed1634d0d26aa7fd772bd1a4b57273a1a34834b
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2022-10-16 17:00:59 -0400

    created a standard vertical menu and added it the Actions 30 panel

---
+++ src/components/UI/VerticalNavMenu.js
@@ -0,0 +1,11 @@
+import classes from "./VerticalNavMenu.module.css";
+
+const VerticalNavMenu = () => {
+  return (
+    <section className={classes.main}>
+      <p>Placeholder</p>
+    </section>
+  );
+};
+
+export default VerticalNavMenu;
