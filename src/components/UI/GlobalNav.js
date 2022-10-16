46ab94927e7de0bde00c2bc32090f6540d47eb36
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2022-10-16 15:17:10 -0400

    Changed CSS for Navlinks to match the old NavTabs.  Deleted Nav tabs since I'm no longer using them.

--- src/components/UI/GlobalNav.js
+++ src/components/UI/GlobalNav.js
@@ -1,32 +1,27 @@
 import classes from "./GlobalNav.module.css";
 import pagoda from "../../assets/lowes-logo.svg";
-import NavTab from "./NavTab";
-import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const GlobalNav = () => {
-
-const linkStyle = ({ isActive }) =>
-isActive ? `${classes.navlink} ${classes.active}` : classes.navlink
+  const linkStyle = ({ isActive }) =>
+    isActive ? `${classes.navlink} ${classes.active}` : classes.navlink;
 
   return (
     <nav className={classes.globalnav}>
-    
       <img className={classes.icon} src={pagoda} alt="Lowes Pagoda" />
-      <ul>
-        <NavLink to="home" className={linkStyle}>
-          Homepage
-        </NavLink>
-        <NavLink to="orders" className={linkStyle}>
-          Orders
-        </NavLink>
-        <NavLink to="showroom" className={linkStyle}>
-          Showroom
-        </NavLink>
-        <NavLink to="returns" className={linkStyle}>
-          Returns
-        </NavLink>
-      </ul>
+
+      <NavLink to="home" className={linkStyle}>
+        Homepage
+      </NavLink>
+      <NavLink to="orders" className={linkStyle}>
+        Orders
+      </NavLink>
+      <NavLink to="showroom" className={linkStyle}>
+        Showroom
+      </NavLink>
+      <NavLink to="returns" className={linkStyle}>
+        Returns
+      </NavLink>
     </nav>
   );
 };
