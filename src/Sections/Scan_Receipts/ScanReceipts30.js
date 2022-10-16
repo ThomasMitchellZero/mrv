0cfb463a2f1b7743792e102a9fe71a6f0017154a
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2022-10-16 17:01:28 -0400

    Created a standard menu and added it to the Actions 30 panel

--- src/Sections/Scan_Receipts/ScanReceipts30.js
+++ src/Sections/Scan_Receipts/ScanReceipts30.js
@@ -1,10 +1,14 @@
 import classes from "./ScanReceipts30.module.css"
 import TitleBar from "../../components/UI/TitleBar";
+import VerticalNavMenu from "../../components/UI/VerticalNavMenu";
+import FooterContainer from "../../components/UI/FooterContainer"
 
 const ScanReceipts30 = (props) => {
   return (
     <div className={`thirty_panel ${classes.scan_receipts_30}`}>
-      <TitleBar>Doubblies</TitleBar>
+      <TitleBar>Actions</TitleBar>
+      <VerticalNavMenu></VerticalNavMenu>
+      <FooterContainer></FooterContainer>
     </div>
   );
 };
