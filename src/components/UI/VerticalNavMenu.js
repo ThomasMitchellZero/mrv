b8889562c736dadb147b6e519dd386f8da7d10c3
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2022-10-18 15:32:39 -0400

    added in 5 new icons to LineIcons component

--- src/components/UI/VerticalNavMenu.js
+++ src/components/UI/VerticalNavMenu.js
@@ -1,9 +1,9 @@
 import classes from "./VerticalNavMenu.module.css";
 
 import VerticalNavButton from "./VerticalNavButton";
+import { ExchangeLineIcon } from "../../assets/lowes-icons/Line-Icons/LineIcons";
 
 const VerticalNavMenu = (props) => {
-
   return (
     <section className={classes.verticalnavmenu}>
       <p>Placeholder</p>
