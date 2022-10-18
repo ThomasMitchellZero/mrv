3b447a081a8db6d892c37f5542b6740f64b2aa2f
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2022-10-17 20:47:13 -0400

    finally got the sizing and fill working correctly for the icon.

--- src/components/UI/VerticalNavButton.js
+++ src/components/UI/VerticalNavButton.js
@@ -5,6 +5,7 @@ import Truck from "../../assets/lowes-icons/Line-Icons/Truck.svg";
 import {
   BoxLineIcon,
   EmailLineIcon,
+  Xtest,
   Ztest,
 } from "../../assets/lowes-icons/Line-Icons/LineIcons";
 import { MdArrowBack } from "react-icons/md";
@@ -13,10 +14,7 @@ const VerticalNavButton = (props) => {
   // const buttonStyle = {}
   return (
     <Link className={classes.main}>
-      <div className={classes.icon1}>
-        <Ztest size="100%" fill="aqua" />
-      </div>
-
+      <Xtest size="24px" fill="gold" className={classes.icon1}></Xtest>
       <p>{props.label}</p>
       <MdArrowBack className={classes.icon2} />
     </Link>
