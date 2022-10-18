29ac11679a70f31dc8d5fb0ff410926c9bc8c5d0
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2022-10-18 16:00:38 -0400

    Vertical Nav Menu buttons are populated and functioning

--- src/components/UI/VerticalNavMenu.js
+++ src/components/UI/VerticalNavMenu.js
@@ -1,13 +1,43 @@
 import classes from "./VerticalNavMenu.module.css";
 
 import VerticalNavButton from "./VerticalNavButton";
-import { ExchangeLineIcon } from "../../assets/lowes-icons/Line-Icons/LineIcons";
+import {
+  ReceiptLineIcon,
+  AddCartLineIcon,
+  LookupLineIcon,
+  SlashCashLineIcon,
+  CloseLineIcon,
+  ExchangeLineIcon,
+} from "../../assets/lowes-icons/Line-Icons/LineIcons";
 
 const VerticalNavMenu = (props) => {
   return (
     <section className={classes.verticalnavmenu}>
       <p>Placeholder</p>
-      <VerticalNavButton label="test"></VerticalNavButton>
+      <VerticalNavButton
+        label="Receipt Entry"
+        mainIcon={<ReceiptLineIcon className={classes.mainicon} />}
+      />
+      <VerticalNavButton
+        label="Item Entry"
+        mainIcon={<AddCartLineIcon className={classes.mainicon} />}
+      />
+      <VerticalNavButton
+        label="Lookup"
+        mainIcon={<LookupLineIcon className={classes.mainicon} />}
+      />
+      <VerticalNavButton
+        label="No Sale"
+        mainIcon={<SlashCashLineIcon className={classes.mainicon} />}
+      />
+      <VerticalNavButton
+        label="Cancel Return"
+        mainIcon={<CloseLineIcon className={classes.mainicon} />}
+      />
+      <VerticalNavButton
+        label="Exchange"
+        mainIcon={<ExchangeLineIcon className={classes.mainicon} />}
+      />
     </section>
   );
 };
