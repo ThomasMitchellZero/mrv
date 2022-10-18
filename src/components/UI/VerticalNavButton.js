d1f9ffefea088120c188944b9a87a60e539914eb
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2022-10-17 21:41:51 -0400

    Custom Icons are now taking fill and classes and scaling as expected

--- src/components/UI/VerticalNavButton.js
+++ src/components/UI/VerticalNavButton.js
@@ -1,12 +1,12 @@
 import classes from "./VerticalNavButton.module.css";
 import { Link } from "react-router-dom";
-import Email from "../../assets/lowes-icons/Line-Icons/Email.svg";
-import Truck from "../../assets/lowes-icons/Line-Icons/Truck.svg";
 import {
   BoxLineIcon,
   EmailLineIcon,
-  Xtest,
-  Ztest,
+  ExchangeLineIcon,
+  PhoneLineIcon,
+  PrintLineIcon,
+  XmarkLineIcon
 } from "../../assets/lowes-icons/Line-Icons/LineIcons";
 import { MdArrowBack } from "react-icons/md";
 
@@ -14,7 +14,7 @@ const VerticalNavButton = (props) => {
   // const buttonStyle = {}
   return (
     <Link className={classes.main}>
-      <Xtest size="24px" fill="gold" className={classes.icon1}></Xtest>
+      <PrintLineIcon className={classes.icon1}/>
       <p>{props.label}</p>
       <MdArrowBack className={classes.icon2} />
     </Link>
