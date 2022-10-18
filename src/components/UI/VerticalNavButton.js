d1e3242ed54280c662c59d8e39ae13ffdd1353d2
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2022-10-18 16:00:29 -0400

    Vertical Nav Menu buttons are populated and functioning

--- src/components/UI/VerticalNavButton.js
+++ src/components/UI/VerticalNavButton.js
@@ -1,26 +1,12 @@
 import classes from "./VerticalNavButton.module.css";
 import { Link } from "react-router-dom";
-import {
-  BoxLineIcon,
-  EmailLineIcon,
-  ExchangeLineIcon,
-  PhoneLineIcon,
-  PrintLineIcon,
-  XmarkLineIcon,
-  AddCartLineIcon,
-  CloseLineIcon,
-  LookupLineIcon,
-  ReceiptLineIcon,
-  SlashCartLineIcon,
-  SlashCashLineIcon,
-} from "../../assets/lowes-icons/Line-Icons/LineIcons";
 import { MdKeyboardArrowRight } from "react-icons/md";
 
 const VerticalNavButton = (props) => {
   // const buttonStyle = {}
   return (
     <Link className={classes.container}>
-      <SlashCashLineIcon className={classes.icon1} />
+      {props.mainIcon}
       <p>{props.label}</p>
       <MdKeyboardArrowRight className={classes.icon2} />
     </Link>
