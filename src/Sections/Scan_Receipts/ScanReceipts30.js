1789f36d4ae5cd0d5bbcf0e5cf95fa753fc7cbb1
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2022-10-22 14:14:29 -0400

    About to do some major restructuring so I want a good save point.

--- src/Sections/Scan_Receipts/ScanReceipts30.js
+++ src/Sections/Scan_Receipts/ScanReceipts30.js
@@ -1,9 +1,9 @@
-import classes from "./ScanReceipts30.module.css"
+import classes from "./ScanReceipts30.module.css";
 
 import TitleBar from "../../components/UI/TitleBar";
 import VerticalNavMenu from "../../components/UI/VerticalNavMenu";
 import VerticalNavButton from "../../components/UI/VerticalNavButton";
-import FooterContainer from "../../components/UI/FooterContainer"
+import FooterContainer from "../../components/UI/FooterContainer";
 
 import {
   ReceiptLineIcon,
@@ -20,35 +20,35 @@ const ScanReceipts30 = (props) => {
     <div className={`thirty_panel ${classes.scan_receipts_30}`}>
       <TitleBar>Actions</TitleBar>
       <VerticalNavMenu>
-      <VerticalNavButton
-        label="Receipt Entry"
-        mainIcon={<ReceiptLineIcon className={classes.mainicon} />}
-      />
-      <VerticalNavButton
-        label="Item Entry"
-        mainIcon={<AddCartLineIcon className={classes.mainicon} />}
-      />
-      <VerticalNavButton
-        label="Lookup"
-        mainIcon={<LookupLineIcon className={classes.mainicon} />}
-      />
-      <VerticalNavButton
-        label="No Sale"
-        to="nosale"
-        mainIcon={<SlashCashLineIcon className={classes.mainicon} />}
-      />
-      <VerticalNavButton
-        label="Cancel Return"
-        mainIcon={<CloseLineIcon className={classes.mainicon} />}
-      />
-      <VerticalNavButton
-        label="Exchange"
-        mainIcon={<ExchangeLineIcon className={classes.mainicon} />}
-      />
-      <VerticalNavButton
-        label="Empty Cart"
-        mainIcon={<SlashCartLineIcon className={classes.mainicon} />}
-      />
+        <VerticalNavButton
+          label="Receipt Entry"
+          mainIcon={<ReceiptLineIcon className={classes.mainicon} />}
+        />
+        <VerticalNavButton
+          label="Item Entry"
+          mainIcon={<AddCartLineIcon className={classes.mainicon} />}
+        />
+        <VerticalNavButton
+          label="Lookup"
+          mainIcon={<LookupLineIcon className={classes.mainicon} />}
+        />
+        <VerticalNavButton
+          label="No Sale"
+          to="nosale"
+          mainIcon={<SlashCashLineIcon className={classes.mainicon} />}
+        />
+        <VerticalNavButton
+          label="Cancel Return"
+          mainIcon={<CloseLineIcon className={classes.mainicon} />}
+        />
+        <VerticalNavButton
+          label="Exchange"
+          mainIcon={<ExchangeLineIcon className={classes.mainicon} />}
+        />
+        <VerticalNavButton
+          label="Empty Cart"
+          mainIcon={<SlashCartLineIcon className={classes.mainicon} />}
+        />
       </VerticalNavMenu>
       <FooterContainer></FooterContainer>
     </div>
