f4e24735fd04767ac509442c750cbf383cdb00b8
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2022-10-23 13:33:12 -0400

    Added in the necessary fields to the Item Number input form.

--- src/Sections/Returns/Scan_Receipts/ItemEntry.js
+++ src/Sections/Returns/Scan_Receipts/ItemEntry.js
@@ -1,18 +1,43 @@
-import classes from "./ItemEntry.module.css"
+import classes from "./ItemEntry.module.css";
 
-import TitleBar from "../../../components/UI/TitleBar"
-import FooterContainer from "../../../components/UI/FooterContainer"
+import TitleBar from "../../../components/UI/TitleBar";
+import FooterContainer from "../../../components/UI/FooterContainer";
 
-const ItemEntry = ()=>{
-    return <form className={`thirty_panel`}>
-        <TitleBar lefticon="back" lefturl="..">Item Entry</TitleBar>
-        <div className={classes.maincontent}>
-
+const ItemEntry = (props) => {
+  return (
+    <form id={props.id} className={`thirty_panel`}>
+      <TitleBar lefticon="back" lefturl="..">
+        Item Entry
+      </TitleBar>
+      <section className={classes.maincontent}>
+        <input
+          type="text"
+          id="item_num"
+          placeholder="Item #, UPC#, or Speed Code"
+          className={`base_input ${classes.itemsearch}`}
+        ></input>
+        <div className={classes.quantity_container}>
+          <label for="item_quantity">Qty.</label>
+          <input
+            type="number"
+            id="item_quantity"
+            placeholder="#"
+            className={`base_input ${classes.itemquantity}`}
+          ></input>
         </div>
-        <FooterContainer>
-            <button type="button" className="baseButton primary large">Add Item</button>
-        </FooterContainer> 
+      </section>
+
+      <FooterContainer>
+        <button
+          form={props.id}
+          type="submit"
+          className="baseButton primary large"
+        >
+          Add Item
+        </button>
+      </FooterContainer>
     </form>
-}
+  );
+};
 
 export default ItemEntry;
