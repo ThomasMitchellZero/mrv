377be8ffe04f1f3c50aa1cd0d736d8880059ac29
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2022-12-24 16:56:24 -0500

    FirstReview now has navigation and it is working properly

--- src/Sections/Returns/Review/FirstReview.js
+++ src/Sections/Returns/Review/FirstReview.js
@@ -1,19 +1,24 @@
-import classes from "./FirstReview.module.css"
+import classes from "./FirstReview.module.css";
 
-import TitleBar from "../../../components/UI/TitleBar"
-import FooterContainer from "../../../components/UI/FooterContainer"
+import TitleBar from "../../../components/UI/TitleBar";
+import FooterContainer from "../../../components/UI/FooterContainer";
+import { useNavigate } from "react-router-dom";
 
-const FirstReview = ()=>{
+const FirstReview = () => {
+  const navigate = useNavigate();
 
-    return <main className={classes.container}>
-        <TitleBar></TitleBar>
-        <section className={classes.mainContent}>
-            <p>we make it here</p>
-
-        </section>
-        <FooterContainer></FooterContainer>
-
+  return (
+    <main className={classes.container}>
+      <TitleBar
+        lefticon={"back"}
+        left_onClick={() => navigate("..")}
+      ></TitleBar>
+      <section className={classes.mainContent}>
+        <p>we make it here</p>
+      </section>
+      <FooterContainer></FooterContainer>
     </main>
-}
+  );
+};
 
-export default FirstReview
+export default FirstReview;
