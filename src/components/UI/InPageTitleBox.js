67825a09ea44f373f8e136695ae00e40979bf169
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2022-12-29 16:12:22 -0500

    InPageTitle now using border divider

--- src/components/UI/InPageTitleBox.js
+++ src/components/UI/InPageTitleBox.js
@@ -1,16 +1,22 @@
 import classes from "./InPageTitleBox.module.css";
 
-const InPageTitle = ({ hasDivider = true, mainTitle ="", subTitle="",topContent, bottomContent}) => {
-
+const InPageTitle = ({
+  hasDivider = true,
+  mainTitle = "",
+  subTitle = "",
+  topContent,
+  bottomContent,
+}) => {
   return (
-    <section className={classes.container}>
+    <section
+      className={`${hasDivider ? "divider-bottom" : ""} ${classes.container}`}
+    >
       <section className={classes.distalHolder}>{topContent}</section>
       <section className={classes.mainContent}>
         <h2 className="inPageTitle">{mainTitle}</h2>
         <h4 className="inPageTitle">{subTitle}</h4>
       </section>
       <section className={classes.distalHolder}>{bottomContent}</section>
-      {hasDivider ? <div className="divider" /> : null}
     </section>
   );
 };
