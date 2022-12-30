200057aafe6dd09320f94ade8d69ccc8fad7abae
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2022-12-30 14:05:03 -0500

    partial work on ReceiptLookup

--- src/Sections/Returns/Receipt_Lookup/ReceiptLookup.js
+++ src/Sections/Returns/Receipt_Lookup/ReceiptLookup.js
@@ -5,25 +5,55 @@ import FooterContainer from "../../../components/UI/FooterContainer";
 import MiniUnmatchedLI from "../../../components/UI/MiniItemLI";
 import MessageRibbon from "../../../components/UI/MessageRibbon";
 
-
+import { useReducer } from "react";
 import { useNavigate, useOutletContext, Link } from "react-router-dom";
+
+
+const defaultState = {
+  activeType: "creditCard",
+  fieldInput:"",
+  inputValid: false,
+  didSearch: false
+}
+
+const lookupReducer = (action, state) => {
+  switch (action.type) {
+    case "SET_SEARCH": {
+      return {...state, activeType: action.payload};
+    }
+
+    case "FIELD_INPUT":{
+      return{}
+    }
+
+    case "MINIMUM_EFFORT":{
+      return{}
+    }
+
+    default:
+      return state;
+  }
+};
 
 const ReceiptLookup = () => {
   const navigate = useNavigate();
 
   const unmatchedArr = Object.values(useOutletContext().session.unmatched);
+  
 
   const unmatchedLIarr = unmatchedArr.map((iObj) => {
     return (
       <MiniUnmatchedLI
         key={iObj.itemNum}
         itemObj={iObj}
-        hasMessage={<MessageRibbon 
-          text="Missing Invoice"
-          size="small"
-          width="auto"
-          color="gold"
-        />}
+        hasMessage={
+          <MessageRibbon
+            text="Missing Invoice"
+            size="small"
+            width="auto"
+            color="gold"
+          />
+        }
       />
     );
   });
