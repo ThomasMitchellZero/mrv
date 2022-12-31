7df0b118d125e2f51a300ab21cd4f82b2f644dff
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2022-12-31 13:52:36 -0500

    changes to the invoice-context appear to be working normally.

--- src/store/invoice-context.js
+++ src/store/invoice-context.js
@@ -1,13 +1,5 @@
 import React from "react";
 
-const [cash, inStoreCredit, applePay, payPal, check] = [
-  "Cash",
-  "In-Store Credit",
-  "ApplePay",
-  "PayPal",
-  "Check",
-];
-
 //products = 100, 200, 300, 400, 900, 910
 
 const InvoiceContext = React.createContext({
@@ -15,12 +7,13 @@ const InvoiceContext = React.createContext({
     invoiceDetails: {
       store: 1234,
       date: new Date(2022, 8, 13),
-      payment: cash,
+      phone: 1231231234,
+      payment: { cash: { paid: 0 } },
     },
     products: {
-      100: { quantity: 8, price: 41.50, tax: 3.80 },
-      300: { quantity: 2, price: 24.15, tax: 1.80 },
-      400: { quantity: 10, price: 13.15, tax: 1.20 },
+      100: { quantity: 8, price: 41.5, tax: 3.8 },
+      300: { quantity: 2, price: 24.15, tax: 1.8 },
+      400: { quantity: 10, price: 13.15, tax: 1.2 },
     },
   },
 
@@ -28,11 +21,21 @@ const InvoiceContext = React.createContext({
     invoiceDetails: {
       store: 1234,
       date: new Date(2022, 1, 22),
-      payment: check,
+      payment: {
+        check: {
+          paid: 0,
+          routing: 12341234,
+          account: 43214321,
+        },
+        credit: {
+          paid: 0,
+          ccNum: 1111222211112222,
+        },
+      },
     },
     products: {
-      200: { quantity: 8, price: 44.00, tax: 4.10 },
-      900: { quantity: 1, price: 987.15, tax: 91.00 },
+      200: { quantity: 8, price: 44.0, tax: 4.1 },
+      900: { quantity: 1, price: 987.15, tax: 91.0 },
     },
   },
 
@@ -40,7 +43,12 @@ const InvoiceContext = React.createContext({
     invoiceDetails: {
       store: 1234,
       date: new Date(2022, 1, 24),
-      payment: check,
+      payment: {
+        credit: {
+          paid: 0,
+          ccNum: 3333444433334444,
+        },
+      },
     },
     products: {
       200: { quantity: 99, price: 44.15, tax: 4.02 },
@@ -51,10 +59,10 @@ const InvoiceContext = React.createContext({
     invoiceDetails: {
       store: 1234,
       date: new Date(2022, 4, 13),
-      payment: check,
+      payment: { cash: { paid: 0 } },
     },
     products: {
-      300: { quantity: 99, price: 24.15, tax: 2.10 },
+      300: { quantity: 99, price: 24.15, tax: 2.1 },
       400: { quantity: 1, price: 1024.15, tax: 98.21 },
     },
   },
@@ -62,14 +70,29 @@ const InvoiceContext = React.createContext({
     invoiceDetails: {
       store: 1234,
       date: new Date(2022, 2, 11),
-      payment: check,
+      payment: { cash: { paid: 0 } },
     },
     products: {
-      100: { quantity: 99, price: 8.15, tax: 0.80 },
+      100: { quantity: 99, price: 8.15, tax: 0.8 },
     },
   },
 
+  FFF: {
+    invoiceDetails: {
+      store: 1234,
+      date: new Date(2022, 3, 8),
+      payment: { cash: { paid: 0 } },
+      orderNum: 11112222,
+      lcaNum: 11112222,
+      proIdNum: 11112222,
+    },
 
+    products: {
+      100: { quantity: 10, price: 8.15, tax: 0.8 },
+      300: { quantity: 8, price: 25.55, tax: 2.1 },
+      400: { quantity: 1, price: 1021.05, tax: 98.21 },
+    },
+  },
 });
 
 export default InvoiceContext;
