205bac289133058c650e1335e62292a43954e9ab
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2023-01-01 10:23:37 -0500

    added customer context to store

---
+++ src/store/customers.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+//products = 100, 200, 300, 400, 900, 910
+
+const CustomerContext = React.createContext({
+
+    111221111: {
+        name: "Nada Vacia"
+    },
+
+    333443333:{
+        name: "Shady McPilferson"
+    },
+
+    555665555:{
+        name: "Ethica Shopwell"
+    },
+})
