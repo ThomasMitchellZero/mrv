22383ae428ef2af85b24c2228d24e77df14d65d2
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2023-01-15 15:56:28 -0500

    TotalReview now reading and listing miniLIs for matched items

--- src/Sections/Returns/Total_Review/TotalReview.js
+++ src/Sections/Returns/Total_Review/TotalReview.js
@@ -3,17 +3,37 @@ import classes from "./TotalReview.module.css";
 import TitleBar from "../../../components/UI/TitleBar";
 import FooterContainer from "../../../components/UI/FooterContainer";
 import InPageTitleBox from "../../../components/UI/InPageTitleBox";
+import MiniItemLI from "../../../components/UI/MiniItemLI";
 
-import { useNavigate, useOutletContext } from "react-router-dom";
+import { useNavigate, useOutletContext, Link } from "react-router-dom";
 
 const TotalReview = () => {
   const returnsContext = useOutletContext();
   const navigate = useNavigate();
+
+  const refItem = {
+    categories: ["Stock"],
+    description: "Two handle classic faucet in wrought iron",
+    disposition: {},
+    itemNum: "100",
+    modelNum: "DR3345",
+    price: 4150,
+    quantity: 2,
+    specialCategories: { ARD: true },
+  };
+
+  const returningItems = Object.values(returnsContext.session.matched);
+  const miniLIarr = returningItems.map((item) => {
+    return <MiniItemLI itemObj={item} totalPrice={true} />;
+  });
+
   return (
     <section className={classes.container}>
       <section className={`thirty_panel`}>
         <TitleBar>Returned Items</TitleBar>
-        <section className={`${classes.mainContent}`}></section>
+        <section className={`${classes.mainContent}`}>
+          {miniLIarr}
+        </section>
         <FooterContainer></FooterContainer>
       </section>
 
