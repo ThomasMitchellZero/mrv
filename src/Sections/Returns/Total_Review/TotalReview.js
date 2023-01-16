984505b6cb8627e164e0b526e7d09c49ace1cf3f
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2023-01-16 14:24:42 -0500

    Refund money now displaying in InPageTitle and RefundTotal in TotalReview page.

--- src/Sections/Returns/Total_Review/TotalReview.js
+++ src/Sections/Returns/Total_Review/TotalReview.js
@@ -4,6 +4,8 @@ import TitleBar from "../../../components/UI/TitleBar";
 import FooterContainer from "../../../components/UI/FooterContainer";
 import InPageTitleBox from "../../../components/UI/InPageTitleBox";
 import MiniItemLI from "../../../components/UI/MiniItemLI";
+import RefundTotal from "../../../components/UI/RefundTotal";
+import Table from "../../../components/UI/Table"
 
 import { useNavigate, useOutletContext, Link } from "react-router-dom";
 
@@ -11,6 +13,7 @@ const TotalReview = () => {
   const returnsContext = useOutletContext();
   const navigate = useNavigate();
 
+  const refund_moneyObj = returnsContext.session.refund_money;
   const returningItems = Object.entries(returnsContext.session.matched);
   const miniLIarr = returningItems.map((item) => {
     return <MiniItemLI key={item[0]} itemObj={item[1]} totalPrice={true} />;
@@ -23,7 +26,9 @@ const TotalReview = () => {
         <section className={`${classes.mainContent} ${classes.thirtyContent}`}>
           {miniLIarr}
         </section>
-        <FooterContainer></FooterContainer>
+        <FooterContainer>
+          <RefundTotal dataObj={refund_moneyObj} hideAdjust={true} />
+        </FooterContainer>
       </section>
 
       <section className={`seventy_panel`}>
@@ -36,7 +41,11 @@ const TotalReview = () => {
           Total Review
         </TitleBar>
         <section className={`${classes.mainContent} ${classes.seventyContent}`}>
-          <InPageTitleBox mainTitle="$XX.XX will refund to the tender(s) below" />
+          <InPageTitleBox
+            mainTitle={`$${(refund_moneyObj.refundTotal / 100).toFixed(
+              2
+            )} will refund to the tender(s) below`}
+          />
         </section>
         <FooterContainer></FooterContainer>
       </section>
