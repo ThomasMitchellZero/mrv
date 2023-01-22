7a0f62e7907e8fbb8f853bf45707ddc958ced623
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2023-01-22 16:55:22 -0500

    added in stepperLine

--- src/Sections/Returns/Finalize_Refund/TenderTypesLI.js
+++ src/Sections/Returns/Finalize_Refund/TenderTypesLI.js
@@ -1,9 +1,20 @@
 import classes from "./TenderTypesLI.module.css";
 
-const TenderTypesLI = ({dataObj}) => {
+import { MdPanoramaFishEye } from "react-icons/md";
+
+const iconsObj = {
+  inProgress: <MdPanoramaFishEye className={`green-text ${classes.icon}`} />,
+};
+
+const TenderTypesLI = ({ dataObj }) => {
   return (
     <section className={classes.container}>
-      <section className={classes.leftColumn}>L</section>
+      <section className={classes.leftColumn}>
+        <div className={`${classes.lineHeight}`}>{iconsObj.inProgress}</div>
+        <div
+          className={`grey-04 ${classes.lineHeight} ${classes.stepLine}`}
+        ></div>
+      </section>
       <section className={classes.mainContent}>M</section>
     </section>
   );
