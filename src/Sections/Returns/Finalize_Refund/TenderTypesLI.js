3fe241ef6f2bc1d0318c319ef15d4e83aa108aa5
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2023-01-24 13:50:23 -0500

    minor TenderLI style updates

--- src/Sections/Returns/Finalize_Refund/TenderTypesLI.js
+++ src/Sections/Returns/Finalize_Refund/TenderTypesLI.js
@@ -15,23 +15,32 @@ const iconsObj = {
   inProgress: <MdPanoramaFishEye className={`green-text ${classes.icon}`} />,
 };
 
+const tableRow = ()=>{
+    
+}
+
 const TenderTypesLI = ({ dataObj }) => {
   return (
-    <section className={classes.container}>
+    <section className={`${classes.container}`}>
       <section className={classes.leftColumn}>
         <div className={`${classes.lineHeight}`}>{iconsObj.inProgress}</div>
         <div
           className={`grey-04 ${classes.lineHeight} ${classes.stepLine}`}
         ></div>
       </section>
-      <section className={`divider-bottom ${classes.mainContent}`}>
-        {/* */}
-        <section className={`${classes.lineHeight}`}>
-          <h5 className={`${classes.XXX}`}>{dataObj.tenderLabel}</h5>
-          <h4>{`$${(dataObj.paid/ 100).toFixed(2)}`}</h4>
-        </section>
-        <section className={`${classes.lineHeight}`}>
-          <h5>{`Not Started`}</h5>
+      <section className={`${classes.fullRow}`}>
+        <section className={`${classes.mainContent}`}>
+          <section className={` ${classes.mainContent}`}>
+            {/* Line 1 */}
+            <section className={`line1 ${classes.lineHeight}`}>
+              <h5 className={`${classes.XXX}`}>{dataObj.tenderLabel}</h5>
+              <h4>{`$${(dataObj.paid / 100).toFixed(2)}`}</h4>
+            </section>
+            <section className={`${classes.lineHeight}`}>
+              <h5>{`Not Started`}</h5>
+            </section>
+            {/* Line 2 */}
+          </section>
         </section>
       </section>
     </section>
