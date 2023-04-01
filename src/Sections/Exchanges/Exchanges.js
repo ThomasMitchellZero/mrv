ca28d7cbda10b3f9ce91110159aed3922abb18e2
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2023-04-01 12:25:55 -0400

    created starting skeleton for Exchanges

--- src/Sections/Exchanges/Exchanges.js
+++ src/Sections/Exchanges/Exchanges.js
@@ -1,7 +1,20 @@
 import "../../mrv-styles/mrvAllStyles.css";
 
+import { useImmer } from "use-immer";
+import produce from "immer";
+
 function Exchanges() {
-  return <section>I'm in.</section>;
+  const [exchSession, setExchSession] = useImmer();
+
+  const handleCLick = () => {
+    console.log("clicked");
+  };
+
+  return (
+    <section className={`mrv mrv-primary-grid`}>
+      I'm in.<button onClick={handleCLick}></button>
+    </section>
+  );
 }
 
 export default Exchanges;
