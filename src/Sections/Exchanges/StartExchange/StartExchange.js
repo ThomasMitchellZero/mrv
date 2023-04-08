d8c8ccfeebc3f58db9bb245cd38293bee7bf5d43
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2023-04-08 18:38:32 -0400

    3 panels positioning on the grid as espected.

--- src/Sections/Exchanges/StartExchange/StartExchange.js
+++ src/Sections/Exchanges/StartExchange/StartExchange.js
@@ -7,25 +7,30 @@ import { useOutletContext } from "react-router-dom";
 function StartExchange() {
   const exchangesCtx = useOutletContext();
   const todosCtx = exchangesCtx.sessionToDo;
-  
-
-  const stateTest = produce(todosCtx, (draftState)=>{
-    console.log(draftState);
-  })
 
   return (
     <section className={`mrv-primary-grid ${classes.container}`}>
-      <div className={`mrv-panel__left mrv-column-grid`}>
-        <Link to={"chooseitems"}>Continue</Link>
+      <div className={`mrv-panel__main MR`}>
+        <div className={`bottomColumnLeft`}>bottoms</div>
+        <div className={`bottomColumnRight`}>bottoms</div>
       </div>
-
-      <div className={`mrv-panel__main mrv-column-grid`}>
-        <div className={`test-bottom-container`}>bottoms</div>
-      </div>
-
-      <div className={`mrv-panel__right mrv-column-grid`}>Side</div>
     </section>
   );
 }
 
 export default StartExchange;
+
+/*
+
+      <div className={`mrv-panel__left`}>
+        <Link to={"chooseitems"}>Continue</Link>
+      </div>
+
+      <div className={`mrv-panel__main`}>
+        <div className={`xxx`}>bottoms</div>
+      </div>
+
+      <div className={`mrv-panel__right`}>Side</div>
+
+
+*/
