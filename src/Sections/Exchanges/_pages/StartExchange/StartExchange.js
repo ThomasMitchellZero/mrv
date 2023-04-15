bdc612a99128493beb564e12cceb8cb6bd74a542
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2023-04-15 15:04:33 -0400

    nm

--- src/Sections/Exchanges/_pages/StartExchange/StartExchange.js
+++ src/Sections/Exchanges/_pages/StartExchange/StartExchange.js
@@ -49,28 +49,4 @@ export default StartExchange;
 
 /*
 
-  XAAA:{
-
-      invoiceDetails: {
-        store: 1234,
-        date: new Date(2022, 3, 8),
-        payment: {
-          44443333: {
-            tenderType: tType.check,
-            tenderLabel: "Check ****3333",
-            paid: 0,
-            routingNum: "44443333",
-            accountNum: "33334444",
-          },
-        },
-        orderNum: "X11111111",
-      },
-
-      products: {
-        900: { quantity: 2, price: 103115, tax: 11030 },
-        910: { quantity: 1, price: 91234, tax: 9120 },
-        400: { quantity: 1, price: 38723, tax: 3810 },
-    },
-  },
-
 */
