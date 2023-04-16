1bbfbf07a3aa2d86bd90958be20d730659befc6d
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2023-04-16 15:44:06 -0400

    ptNode text now correctly overflowing body

--- src/mrv/mrv-components/mrvPizzaTracker/PTnode.js
+++ src/mrv/mrv-components/mrvPizzaTracker/PTnode.js
@@ -1,10 +1,22 @@
 import classes from "./ptStyle.module.css";
 
-function PTnode({dataObj, nodeIndex = 99}) {
+function PTnode({ dataObj, nodeIndex = 99 }) {
+  const status = "notStarted";
+
+  const statusObj = {
+    notStarted: { css: classes.notStarted },
+    active: { css: classes.active },
+    complete: { css: classes.complete },
+  };
+
   return (
-    <section>
-      <button className={``}>{nodeIndex}</button>
-      <p className={``}>{dataObj.title}</p>
+    <section className={`${classes.ptNode}`}>
+      <button
+        className={`body__small bold ${classes.ptBtn} ${statusObj[status].css}`}
+      >
+        {nodeIndex}
+      </button>
+        <p className={`body__small bold`}>{dataObj.title}</p>
     </section>
   );
 }
