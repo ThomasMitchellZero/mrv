4326806162d0bcfb3f2281b216eaaafca8bc5d77
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2023-04-20 18:01:24 -0400

    nm

--- src/Sections/Exchanges/_pages/StartExchange/StartExchange.js
+++ src/Sections/Exchanges/_pages/StartExchange/StartExchange.js
@@ -5,6 +5,7 @@ import { ExchHeader } from "../../_Resources/components/ExchHeader";
 import { actPan } from "../../_Resources/glossary/glossaryExch";
 import { useSetActivePanels } from "../../_Resources/customHooks/useSetActivePanels";
 import InvoiceContext from "../../../../store/invoice-context";
+import ProductContext from "../../../../store/product-context";
 
 import { useContext } from "react";
 import { useOutletContext, useNavigate } from "react-router-dom";
@@ -13,15 +14,26 @@ function StartExchange() {
   const exchCtx = useOutletContext();
   const setExchState = exchCtx.setExchSession;
   const invoiceContext = useContext(InvoiceContext);
+  const productContext = useContext(ProductContext);
+
   const nextAp = actPan.selectItems;
 
   const setActivePanels = useSetActivePanels();
   const navigate = useNavigate();
 
+  /* ---- SHARED FUNCTIONS ---- */
+
   const handleSetInvoice = (invoNum) => {
     setExchState((draft) => {
+
       draft.activeOrder = invoiceContext[invoNum].invoiceDetails.orderNum;
       draft.invoiceProducts = invoiceContext[invoNum].products;
+
+      for(const i of Object.keys(draft.invoiceProducts)){
+        const details = productContext[i];
+        draft.invoiceProducts[i].productDetails = details;
+      }
+
     });
   };
 
@@ -30,6 +42,8 @@ function StartExchange() {
     navigate("chooseitems");
     setActivePanels({ main: nextAp.invoItemsTable.k });
   };
+
+  /* ---- OUTPUT JSX ---- */
 
   return (
     <section className={`mrvPage ${classes.container}`}>
