7ac2390c39218a3ed9bb1ce5c84fba073452e530
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2023-04-29 14:55:55 -0400

    Got it working,  now just need to do some testing to find out what went wrong.

--- src/Sections/Exchanges/Exchanges.js
+++ src/Sections/Exchanges/Exchanges.js
@@ -6,12 +6,46 @@ import { defaultExchState } from "./_Resources/glossary/glossaryExch";
 
 import { Outlet } from "react-router";
 
+let testData = [];
+
+for (let i = 0; i < 33; i++) {
+  testData.push(
+    <tr>
+      <td>{i}</td>
+    </tr>
+  );
+}
+
 function Exchanges() {
   const [exchSession, setExchSession] = useImmer({
     ...defaultExchState,
+    test: testData,
   });
 
+  const testTable = (
+    <table>
+      <thead>
+        <tr>
+          <th>This is just a test</th>
+        </tr>
+      </thead>
+      <tbody>{testData}</tbody>
+    </table>
+  );
+
   return (
+    <section className={`mrv-top`}>
+      <section className={`SecondLeveltest`}>
+        <section className={`testContainerTest`}>{testTable}</section>
+      </section>
+    </section>
+  );
+}
+
+export default Exchanges;
+
+/*
+
     <section className={`mrv-top`}>
       <Outlet
         context={{
@@ -20,11 +54,5 @@ function Exchanges() {
         }}
       />
     </section>
-  );
-}
-
-export default Exchanges;
-
-/*
 
 */
