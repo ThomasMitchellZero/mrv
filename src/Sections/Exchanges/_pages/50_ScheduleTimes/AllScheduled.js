b28a8595683ce3f573d9c7284e8a4c526a6ad723
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2023-05-06 14:24:39 -0400

    Some CSS organization

--- src/Sections/Exchanges/_pages/50_ScheduleTimes/AllScheduled.js
+++ src/Sections/Exchanges/_pages/50_ScheduleTimes/AllScheduled.js
@@ -8,38 +8,36 @@ import { MdArrowForward, MdOutlineCheckCircle } from "react-icons/md";
 
 import { useOutletContext } from "react-router";
 
-function AllScheduled({ localSt, setLocSt }) {
+function AllScheduled({ parentSt, setParSt }) {
   const exchCtx = useOutletContext();
   const setExchState = exchCtx.setExchSession;
   const exchProdsMap = exchCtx.exchSession.exchProducts;
 
-  const activeKey = localSt.activeKey;
-  const show30warning = localSt.show30warning;
-  const activeTimeBtnObj = localSt.activeTimeBtnObj;
+  const activeKey = parentSt.activeKey;
+  const show30warning = parentSt.show30warning;
+  const activeTimeBtnObj = parentSt.activeTimeBtnObj;
   const activeProduct = exchProdsMap.get(activeKey);
 
   const allComplete = true;
 
   /* ---- Shared Functions ---- */
 
-
   const handleContinue = () => {
-
-    const pickedTime = localSt.activeTimeBtnObj;
+    const pickedTime = parentSt.activeTimeBtnObj;
 
     //If a time was picked...
     if (pickedTime) {
       // add that time to the
       setExchState((draft) => {
-        const activeProduct = localSt.activeKey;
+        const activeProduct = parentSt.activeKey;
         draft.exchProducts.get(activeProduct).apptTime = pickedTime;
       });
 
-      setLocSt((draft)=>{
+      setParSt((draft) => {
         draft.activeKey = null;
         //draft.activeTimeBtnObj = null;
         draft.showApplyWarning = false;
-      })
+      });
     }
   };
 
@@ -49,19 +47,22 @@ function AllScheduled({ localSt, setLocSt }) {
 
   return (
     <>
-      <section className={` main_content main_col ${classes.allScheduled}`}>
-        Hello there
+      <section className={` main_col ${classes.allScheduled}`}>
+        <MdOutlineCheckCircle />
+        <h4 className={`heading__small`}>
+          All exchanges have been scheduled.
+        </h4>
       </section>
       <section className={`footer_text right_col`}>
         <p className={`tinyText warning`}></p>
       </section>
       <section className={`footer_content right_col`}>
-          <button
-            onClick={handleContinue}
-            className={`mrvBtn primary fullWidth jumbo`}
-          >
-            Continue
-          </button>
+        <button
+          onClick={handleContinue}
+          className={`mrvBtn primary fullWidth jumbo`}
+        >
+          Continue
+        </button>
       </section>
     </>
   );
