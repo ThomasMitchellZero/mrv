a536fb69c791eb448333a4ab7737a34cfbe33822
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2023-10-03 16:31:18 -0400

    Updates to Money Row and ReplacementDetails30

--- src/Sections/Exchanges/_Resources/components/displayOutputs/MoneyRow.js
+++ src/Sections/Exchanges/_Resources/components/displayOutputs/MoneyRow.js
@@ -4,14 +4,20 @@ function MoneyRow({
   title = "",
   money = "",
   textColorClass = "color__primary__text",
+  moneyColorClass = textColorClass,
   bigMoney = false,
 }) {
+
+  const moneyOutput = bigMoney ? (
+    <h4 className={` ${textColorClass}`}>{`$ ${money}`}</h4>
+  ) : (
+    <p className={`body__large ${textColorClass}`}>{`$ ${money}`}</p>
+  );
+
   return (
     <section className={`${classes.container}`}>
       <p className={`body ${textColorClass} ${classes.titleText}`}>{title}</p>
-      <p className={`body__large ${textColorClass}`}>
-        {`$ ${money}`}
-      </p>
+      {moneyOutput}
     </section>
   );
 }
--- src/Sections/Exchanges/_pages/40_ExchReplacements/ReplacementDetails30.js
+++ src/Sections/Exchanges/_pages/40_ExchReplacements/ReplacementDetails30.js
@@ -1,6 +1,7 @@
 import { ExchHeader } from "../../_Resources/components/pageLayout/ExchHeader";
 import { useExchNav } from "../../_Resources/customHooks/useExchNav";
 import { MoneyRow } from "../../_Resources/components/displayOutputs/MoneyRow";
+import { ProductInfo } from "../../_Resources/components/displayOutputs/ProductInfo";
 import { useOutletContext } from "react-router";
 
 function ReplacementDetails30({ parLocSt_Replace, setParLocSt_Replace }) {
@@ -9,18 +10,24 @@ function ReplacementDetails30({ parLocSt_Replace, setParLocSt_Replace }) {
   const exchItems = exchCtx.exchSession.itemsInExchange;
   const exchDeliveries = exchCtx.exchSession.deliveryGroups;
 
+
+
   const exchNav = useExchNav();
 
   const handleApply = () => {};
 
   return (
-    <section className={`mrvPanel__side `}>
+    <section className={`mrvPanel__side RI_Panel30 `}>
       <ExchHeader
         hasCluster={false}
         headerTitle={"ReplacementItems"}
         hasProductName={false}
       />
-      <section className={`main_content`}></section>
+      <section className={`main_content`}>
+        <ProductInfo itemObj={exchItems["400"].returningItem} />
+        <div className={`divider horizontal`} />
+        <MoneyRow />
+      </section>
       <section className={`footer_text`}>
         <p className={`tinyText warning`}></p>
       </section>
